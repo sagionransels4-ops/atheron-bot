d757ccc396fb589ff90a9725331a6e4ba74360ac
sagionransels4-ops <sagionransels4@gmail.com>
2026-04-21 05:43:42 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log('?? Atheron Bot está encendido');
+});
+
+client.on('messageCreate', message => {
+  if (message.content === '!hola') {
+    message.reply('?? Atheron activo');
+  }
+});
+
+client.login(process.env.TOKEN);
