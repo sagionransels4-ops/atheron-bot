cb62493011a367f3d7b2b31bd9c15435404c46c2
sagionransels4-ops <sagionransels4@gmail.com>
2026-04-21 10:45:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -26,25 +26,3 @@ client.on('messageCreate', (message) => {
   }
 });
 
-const sesiones = {};
-
-const preguntas = [
-"¿Cuál es tu experiencia como staff?",
-"¿Has moderado antes?",
-"¿Cuántas horas estás activo?",
-"¿Cómo manejas conflictos?",
-"¿Qué harías si un usuario insulta?"
-];
-
-client.on('messageCreate', (message) => {
-  if (message.content === '!formulario') {
-
-    sesiones[message.author.id] = {
-      paso: 0,
-      respuestas: []
-    };
-
-    message.channel.send("📋 Formulario iniciado");
-    message.channel.send(preguntas[0]);
-  }
-});
